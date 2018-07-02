a0735188f5261fddf908320cc4d74725410af8de
HubertusKaiser <hubertus.kaiser@to-dortmund.de>
2018-07-02 12:32:56 +0200

    V49

--- V49/PythonSkript.py
+++ V49/PythonSkript.py
@@ -1,7 +1,5 @@
 ##################################################### Import system libraries ######################################################
 import matplotlib as mpl
-mpl.rcdefaults()
-mpl.rcParams.update(mpl.rc_params_from_file('meine-matplotlibrc'))
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.constants as const
@@ -48,172 +46,129 @@ from error_calculation import(
 from utility import(
     constant
 )
-################################################ Finish importing custom libraries #################################################
+from scipy.optimize import curve_fit
+##############Arrays die nachher benötigt werden########
+t_0=np.array([])
+U_1=np.array([])
+U_2=np.array([])
+U_fit=np.array([])
+
+##################################Berechnung von T1#########################################
+#Laden der Daten
+tau, A = np.genfromtxt("messdaten/t1_bestimmung.txt", unpack=True)
+
+def fitfunction(tau, a, T1, b):
+     return -a * ( 1 -( 2 * np.exp(-tau/T1)))+b
+
+params_1, covariance_1 = curve_fit( fitfunction, tau, A , maxfev=900000000)
+errors_1 = np.sqrt(np.diag(covariance_1))
+#Plotten der Daten
+x_plot = np.linspace(0, 5000 ,10000)
+plt.plot(x_plot, fitfunction(x_plot, *params_1), "r-", label="Fit")
+plt.plot( tau, A, ".c",label="Messwerte zur Bestimmung von Tau_1")
+plt.xlabel('tau in ms')
+plt.ylabel('A in mV')
+plt.legend(loc="best")
+plt.grid()
+plt.savefig("build/fit_tau_1.pdf")
+plt.clf()
 
 
 
-#Dummy_linie=np.array([431,412,431,492,409,478,414,456])
-#Dummy_punkt_m=np.array([417,433,445,471,432,441,434,431])
-#Dummy_punkt_r=np.array([410,365,370,578,356,570,355,456])
-#Glas_linie=np.array([307,328,321,349,356,380,402,415])
-#x=np.array([1,2,3,4,5,6,7,8])
+###############################################Tau2 Bestimmen
+t_xaxsis, U1, U2 =np.genfromtxt('messdaten/scope_t2.csv', delimiter=',',comments="#", unpack=True)
+U2=-1*U2
+def fitfunction_tau2(tau_values, a, b, c):
+     return -a * ( np.exp(-tau_values/b))+c
 
-#Dummy_linie_1=np.array([447.34,408.43,451.84,439.89,442.77,474.77,466.83,458.45])
-#Dummy_punkt_1=np.array([428.37,422.61,440.41,463.61,432.89,453.08,452.58,441.28])
-#Dummy_linie_2=np.array([431.78,412.49,431.88,492.57,409.78,478.85,414.18,456.49])
-#Dummy_punkt_2=np.array([417.90,433.27,445.01,471.99,432.69,441.12,434.40,431.51])
-#Dummy_linie_3=np.array([404.51,392.85,449.32,422.04,404.41,408.52,445.85,457.21])
-#Dummy_punkt_3=np.array([430.17,411.86,434.47,446.10,430.95,413.67,423.92,441.78])
+params_2, covariance_2 = curve_fit( fitfunction_tau2, t_xaxsis, U2 , maxfev=900000000)
+errors_2 = np.sqrt(np.diag(covariance_2))
+plt.clf()
+
+plt.plot(t_xaxsis,U2,".k")
+x_plot_tau2=np.linspace(0,1.905,10000)
+plt.plot(x_plot_tau2, fitfunction_tau2(x_plot_tau2, *params_2), "r-", label="Fit_Tau2")
+plt.xlabel('tau in ms')
+plt.ylabel('A in V')
+plt.grid()
+plt.savefig("build/U_fit_Tau2.pdf")
+plt.clf()
 
 
-#plt.plot(x, Dummy_linie, 'rx', label='Dummy-Linie')
-#plt.plot(x, Dummy_punkt_r, 'bx', label='Dummy-Punkt-Rand')
-#plt.plot(x, Dummy_punkt_m, 'gx', label='Dummy-Punkt-Mitte')
-#plt.plot(x, Glas_linie, 'ko', label='Glas-Linie')
-## plt.plot(x, Dummy_punkt_1, 'ro', label='Dummy-Punkt-1')
-## plt.plot(x, Dummy_punkt_2, 'bo', label='Dummy-Punkt-2')
-## plt.plot(x, Dummy_punkt_3, 'go', label='Dummy-Punkt-3')
-#plt.axhline(y=420, xmin=0, xmax=8, linewidth=2, color = 'm', label='Soll')
-#plt.xlabel(r'$\text{Anzahl Asics}$')
-#plt.ylabel(r'$\text{Hoehe} \:\text{in}\: \text{µm}$')
-#plt.legend(loc='best')
-#plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-#plt.savefig('build/Asics.pdf')
+
+###########Bestimmung der Diffusionskonstante D #################
+#es sieht so aus als ist irgenwas falsch gepolt, da bei uns vorzeichen und ähnliches Vertauscht sind
+
+td, Udiff = np.genfromtxt("messdaten/diff_const.txt", unpack=True)
+def fitfunction_D(td,d, a,c,b):
+     return -a * (np.exp(-td/d)*np.exp(-b* td**3))+c
+Udiff=Udiff
+p0=[-10,700, 10,0.0002]
+params_3, covariance_3 = curve_fit( fitfunction_D, td, Udiff
+,p0=p0
+, maxfev=999000000)
+errors_3 = np.sqrt(np.diag(covariance_2))
+
+x_range=np.linspace(5,31,10000)
+plt.plot(td,Udiff,".r")
+plt.plot(x_range, fitfunction_D(x_range, *params_3), "r-", label="Fit_D")
+plt.plot( td, Udiff, ".b",label="Messwerte zur Bestimmung von D")
+plt.xlabel('tau in ms')
+plt.grid()
+plt.ylabel('A in mV')
+plt.legend(loc="best")
+plt.savefig("build/plot_diff.pdf")
+plt.clf()
+
+
+##Bestimmung der Halbwertsbreite
+t_w, U1, U2 =np.genfromtxt('messdaten/scope_1.csv', delimiter=',', unpack=True)
+plt.plot(t_w, U2, ".c")
+plt.grid()
+plt.axvline(x=0.020127177, c="black")
+plt.axvline(x=0.020010, c="black")
+plt.axhline(y=0.56177, c="blue")
+plt.axhline(y=0.28085, c="green")
+plt.axhline(y=0.22085, xmin=0.020, xmax=0.0201,c="red")
+plt.axhline(y=0.0, c="blue")
+plt.savefig("build/halbw.pdf")
+plt.clf()
+
+
+
+####Plot der Parameter um diese abzuschätzen
+#x_range_plot=np.linspace(0,31,1000)
+#a,c,b,d=p0
+#values=a * (np.exp(-x_range_plot/T2)*np.exp(-b* x_range_plot**3))+c
+#plt.plot(x_range_plot, values, ".k")
+#plt.savefig("build/paraschäter.pdf")
 #plt.clf()
 
-#plt.plot(x, Dummy_linie_1, 'ro', label='Dummy-linie-1')
-#plt.plot(x, Dummy_linie_2, 'bo', label='Dummy-linie-2')
-#plt.plot(x, Dummy_linie_3, 'go', label='Dummy-linie-3')
-#plt.axhline(y=420, xmin=0, xmax=8, linewidth=2, color = 'm', label='Soll')
-#plt.xlabel(r'$\text{Anzahl Asics}$')
-#plt.ylabel(r'$\text{Hoehe} \:\text{in}\: \text{µm}$')
-#plt.legend(loc='best')
-#plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-#plt.savefig('build/Asics_1.pdf')
-
-################################ FREQUENTLY USED CODE ################################
-#
-########## IMPORT ##########
-# t, U, U_err = np.genfromtxt('messdaten/data.txt', unpack=True)
-# t *= 1e-3
 
 
-########## ERRORS ##########
-# R_unc = ufloat(R[0],R[2])
-# U = 1e3 * unp.uarray(U, U_err)
-# Rx_mean = np.mean(Rx)                 # Mittelwert und syst. Fehler
-# Rx_mean_with_error = mean(Rx, 0)      # unp.uarray mit Fehler und Fehler des Mittelwertes, die 0 gibt an, dass in einem R^2 array jeweils die Zeilen gemittelt werden sollen
-# Rx_mean_err = MeanError(noms(Rx))     # nur der Fehler des Mittelwertes
-#
-## Relative Fehler zum späteren Vergleich in der Diskussion
-# RelFehler_G = (G_mess - G_lit) / G_lit
-# RelFehler_B = (B_mess - B_lit) / B_lit
-# write('build/RelFehler_G.tex', make_SI(RelFehler_G*100, r'\percent', figures=1))
-# write('build/RelFehler_B.tex', make_SI(RelFehler_B*100, r'\percent', figures=1))
+
+#Print der Parameter T1
+print("Parameter der Fits, für die T1 Bestimmung")
+print("a=0", params_1[0], "±", errors_1[0])
+print("T_1=0", params_1[1], "±", errors_1[1])
+
+#Print der Parameter T2
+print("Parameter der Fits zur Bestimmung von T2")
+print("U_0=0", params_2[0], "±", errors_2[0])
+print("T_2=0", params_2[1], "±", errors_2[1])
+print("c", params_2[2], "±", errors_2[2])
+
+#Print der Parameter  D
+print("Parameter der Fits von D")
+print("a", params_3[0], "±", errors_3[0])
+print("b", params_3[1], "±", errors_3[1])
+print("c", params_3[2], "±", errors_3[2])
 
 
-########## CURVE FIT ##########
-# def f(t, a, b, c, d):
-#     return a * np.sin(b * t + c) + d
-#
-# params = ucurve_fit(f, t, U, p0=[1, 1e3, 0, 0])   # p0 bezeichnet die Startwerte der zu fittenden Parameter
-# params = ucurve_fit(reg_linear, x, y)             # linearer Fit
-# params = ucurve_fit(reg_quadratic, x, y)          # quadratischer Fit
-# params = ucurve_fit(reg_cubic, x, y)              # kubischer Fit
-# a, b = params
-# write('build/parameter_a.tex', make_SI(a * 1e-3, r'\kilo\volt', figures=1))       # type in Anz. signifikanter Stellen
-# write('build/parameter_b.tex', make_SI(b * 1e-3, r'\kilo\hertz', figures=2))      # type in Anz. signifikanter Stellen
-
-########## MAKE_SI ##########
-# make_SI(m_eff_n*1e32, r'\kilo\gramm', figures=1, exp='e-32')
-# ufloats, die einen exponenten besitzen, werden als (2.3+/-0.3)e-32 dargestellt, das Problem ist hier der Exponent,
-# man sollte sich bei einem Fehler der Form:
-# TypeError: non-empty format string passed to object.__format__
-# den Wert über print() ausgeben lassen und dann obige Modifikation (links *1e32, und dann exp='e-32') nutzen.
-# (hierzu gibt es momentan /Juni2017 ein issue)
 
 
-########## PLOTTING ##########
-# plt.clf()                   # clear actual plot before generating a new one
-#
-## automatically choosing limits with existing array T1
-# t_plot = np.linspace(np.amin(T1), np.amax(T1), 100)
-# plt.xlim(t_plot[0]-1/np.size(T1)*(t_plot[-1]-t_plot[0]), t_plot[-1]+1/np.size(T1)*(t_plot[-1]-t_plot[0]))
-#
-## hard coded limits
-# t_plot = np.linspace(-0.5, 2 * np.pi + 0.5, 1000) * 1e-3
-#
-## standard plotting
-# plt.plot(t_plot * 1e3, f(t_plot, *noms(params)) * 1e-3, 'b-', label='Fit')
-# plt.plot(t * 1e3, U * 1e3, 'rx', label='Messdaten')
-## plt.errorbar(B * 1e3, noms(y) * 1e5, fmt='rx', yerr=stds(y) * 1e5, label='Messdaten')        # mit Fehlerbalken
-## plt.xscale('log')                                                                            # logarithmische x-Achse
-# plt.xlim(t_plot[0] * 1e3, t_plot[-1] * 1e3)
-# plt.xlabel(r'$t \:/\: \si{\milli\second}$')
-# plt.ylabel(r'$U \:/\: \si{\kilo\volt}$')
-# plt.legend(loc='best')
-# plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-# plt.savefig('build/aufgabenteil_a_plot.pdf')
 
 
-########## WRITING TABLES ##########
-### IF THERE IS ONLY ONE COLUMN IN A TABLE (workaround):
-## a=np.array([Wert_d[0]])
-## b=np.array([Rx_mean])
-## c=np.array([Rx_mean_err])
-## d=np.array([Lx_mean*1e3])
-## e=np.array([Lx_mean_err*1e3])
-#
-# write('build/Tabelle_b.tex', make_table([a,b,c,d,e],[0, 1, 0, 1, 1]))     # fehlerbehaftete arrays (uarrays) sollten rechts eine 1 bekommen (1 signifikante Stelle)
-# write('build/Tabelle_b_texformat.tex', make_full_table(
-#     caption = 'Messdaten Kapazitätsmessbrücke.',
-#     label = 'table:A2',
-#     source_table = 'build/Tabelle_b.tex',
-#     stacking = [1,2,3,4,5],              # Hier aufpassen: diese Zahlen bezeichnen diejenigen resultierenden Spaltennummern, die Multicolumns sein sollen
-#     units = ['Wert',
-#     r'$C_2 \:/\: \si{\nano\farad}$',
-#     r'$R_2 \:/\: \si{\ohm}$',
-#     r'$R_3 / R_4$', '$R_x \:/\: \si{\ohm}$',
-#     r'$C_x \:/\: \si{\nano\farad}$'],
-#     replaceNaN = True,                      # default = false
-#     replaceNaNby = 'not a number'))         # default = '-'
-#
-## Aufsplitten von Tabellen, falls sie zu lang sind
-# t1, t2 = np.array_split(t * 1e3, 2)
-# U1, U2 = np.array_split(U * 1e-3, 2)
-# write('build/loesung-table.tex', make_table([t1, U1, t2, U2], [3, None, 3, None]))  # type in Nachkommastellen
-#
-## Verschmelzen von Tabellen (nur Rohdaten, Anzahl der Spalten muss gleich sein)
-# write('build/Tabelle_b_composed.tex', make_composed_table(['build/Tabelle_b_teil1.tex','build/Tabelle_b_teil2.tex']))
-
-
-########## ARRAY FUNCTIONS ##########
-# np.arange(2,10)                   # Erzeugt aufwärts zählendes Array von 2 bis 10
-# np.zeros(15)                      # Erzeugt Array mit 15 Nullen
-# np.ones(15)                       # Erzeugt Array mit 15 Einsen
-#
-# np.amin(array)                    # Liefert den kleinsten Wert innerhalb eines Arrays
-# np.argmin(array)                  # Gibt mir den Index des Minimums eines Arrays zurück
-# np.amax(array)                    # Liefert den größten Wert innerhalb eines Arrays
-# np.argmax(array)                  # Gibt mir den Index des Maximums eines Arrays zurück
-#
-# a1,a2 = np.array_split(array, 2)  # Array in zwei Hälften teilen
-# np.size(array)                    # Anzahl der Elemente eines Arrays ermitteln
-#
-# np.delte(A,3)                     # liefert ein Array, in dem der Eintrag mit Index 3 des arrays
-                                    # A gelöscht wurde (nachfolgende indices werden aufgerückt)
-
-
-########## ARRAY INDEXING ##########
-# y[n - 1::n]                       # liefert aus einem Array jeden n-ten Wert als Array
-
-
-########## DIFFERENT STUFF ##########
-# R = const.physical_constants["molar gas constant"]      # Array of value, unit, error, siehe https://docs.scipy.org/doc/scipy-0.19.0/reference/constants.html
-# search_replace_within_file('build/Tabelle_test.tex','find me','found you')    # Selbsterklärend
-
-
-## convenience file writing for standard make Files
 f = open('build/.pysuccess', 'w')
 f.write('MarktkraM')
 f.close()
---
+++ V49/pythontest.py
@@ -0,0 +1,72 @@
+##################################################### Import system libraries ######################################################
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.constants as const
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+from uncertainties.unumpy import (
+    nominal_values as noms,
+    std_devs as stds,
+)
+################################################ Finish importing system libraries #################################################
+
+################################################ Adding subfolder to system's path #################################################
+import os, sys, inspect
+# realpath() will make your script run, even if you symlink it :)
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+
+ # use this if you want to include modules from a subfolder
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"python_custom_scripts")))
+if cmd_subfolder not in sys.path:
+    sys.path.insert(0, cmd_subfolder)
+############################################# Finish adding subfolder to system's path #############################################
+
+##################################################### Import custom libraries ######################################################
+from curve_fit import ucurve_fit
+from table import (
+    make_table,
+    make_full_table,
+    make_composed_table,
+    make_SI,
+    write,
+    search_replace_within_file,
+)
+from regression import (
+    reg_linear,
+    reg_quadratic,
+    reg_cubic
+)
+from error_calculation import(
+    mean,
+    MeanError
+)
+from utility import(
+    constant
+)
+from scipy.optimize import curve_fit
+
+################################################ Finish importing custom libraries #################################################
+
+
+t_0=np.array([])
+U_1=np.array([])
+U_2=np.array([])
+U_va=np.array([])
+U_fit=np.array([])
+
+
+
+
+
+###############################################Tau2 Bestimmen
+
+
+
+
+
+f = open('build/.pysuccess', 'w')
+f.write('MarktkraM')
+f.close()
